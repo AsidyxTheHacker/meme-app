bb659904977a775e995063ccf8da60b70e59d159
Adam <adammiller.2000nj@gmail.com>
2023-12-06 15:44:40 -0500

    added base variables

---
+++ script.js
@@ -0,0 +1,6 @@
+let meme = document.getElementById('meme');
+let title = document.getElementById('title');
+let memeBtn = document.getElementById('meme-btn');
+let url = 'https://meme-api.com/gimme/';
+
+let subreddits = ["okbuddyretard", "fffffffuuuuuuuuuuuu", "dankmemes", "dogmemes", "me_irl"];
