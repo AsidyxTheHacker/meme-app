edd151e499652c515a20260627671e3b7f9992f4
Adam <adammiller.2000nj@gmail.com>
2023-12-06 16:02:46 -0500

    added function for meme to load on button press

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let memeBtn = document.getElementById('meme-btn');
 let url = 'https://meme-api.com/gimme/';
 
 let subreddits = ["okbuddyretard", "fffffffuuuuuuuuuuuu", "dankmemes", "dogmemes", "me_irl"];
+
+let getMemes = () => {
+    let randomNum = Math.floor(Math.random() * subreddits.length);
+    let getSub = subreddits[randomNum];
+    
+    fetch(url + getSub).then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        let memeImg = new Image();
+        memeImg.onload = () => {
+            meme.src = data.url;
+            title.innerText = data.title;
+        };
+        memeImg.src = data.url
+    })
+}
+
+memeBtn.addEventListener('click', getMemes);
+window.addEventListener('load', getMemes);
